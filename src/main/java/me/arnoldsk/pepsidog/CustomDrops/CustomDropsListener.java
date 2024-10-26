8bd7c80df92a6da31de02e2cb3e88be029005369
ArnoldsK <arnolds0klavenieks@gmail.com>
2024-10-26 22:42:08 +0300

    Drop elytra on dragon kill instead of an award

--- src/main/java/me/arnoldsk/pepsidog/CustomDrops/CustomDropsListener.java
+++ src/main/java/me/arnoldsk/pepsidog/CustomDrops/CustomDropsListener.java
@@ -1,13 +1,10 @@
 package me.arnoldsk.pepsidog.CustomDrops;
 
 import me.arnoldsk.pepsidog.PepsiDog;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.EnderDragon;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -25,31 +22,14 @@ public class CustomDropsListener implements org.bukkit.event.Listener {
 
         // Has to be a dragon
         if (!(entity instanceof EnderDragon)) return;
-
         EnderDragon dragon = (EnderDragon) entity;
 
-        // Has to be killed by a player
-        Player player = dragon.getKiller();
-
-        if (player == null) return;
-
-        playerAwardElytra(player);
+        // Drop the Elytra
+        ItemStack elytra = new ItemStack(Material.ELYTRA);
+        ItemMeta meta = elytra.getItemMeta();
+        assert meta != null;
+        meta.setDisplayName("Ender Dragon Elytra");
+        elytra.setItemMeta(meta);
+        dragon.getWorld().dropItem(dragon.getLocation(), elytra);
     }
-
-        void playerAwardElytra(Player player) {
-            ItemStack elytra = new ItemStack(Material.ELYTRA);
-            ItemMeta meta = elytra.getItemMeta();
-
-            // Give the Elytra a custom name
-            if (meta != null) {
-                meta.setDisplayName("Ender Dragon Elytra");
-                elytra.setItemMeta(meta);
-            }
-
-            // Give the item
-            player.getInventory().addItem(elytra);
-
-            // Alert
-            plugin.getServer().broadcastMessage(ChatColor.GREEN + player.getName() + " has received Elytra from the Ender Dragon");
-        }
 }
